de09dd5536af8fd4cedf3a9534aae5cbc2e0ed95
Павел Королев <p.koror56@gmail.com>
2026-09-05 12:14:45 +0500

    feat(T-013): компонент загрузки CSV с drag-and-drop, прогрессом и результатом

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Families from './pages/Families';
 import FamilyDetail from './pages/FamilyDetail';
+import Upload from './pages/Upload';
 import PrivateRoute from './components/PrivateRoute';
 import Layout from './components/Layout';
 
@@ -44,6 +45,16 @@ export default function App() {
           <PrivateRoute>
             <Layout>
               <FamilyDetail />
+            </Layout>
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/upload"
+        element={
+          <PrivateRoute>
+            <Layout>
+              <Upload />
             </Layout>
           </PrivateRoute>
         }
